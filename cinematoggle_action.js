440fbe9e243b23d8ac1726475bf6d91a6d6e60c2
Ian <ibryan@uw.edu>
2020-07-07 23:10:11 -0700

    Removed unused code

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -13,18 +13,9 @@
 function toggleCinema () {
   createButtons: function() {
       $('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>').appendTo("body").click(function() {
-          if (!$("body").hasClass("cinemachat")) {
-              if ($("#userlist").is(":visible")) {
-                  $("#userlisttoggle").click()
-              }
-          }
+
           $("body").toggleClass("cinemachat");
-          if ($("iframe[src*=livestream]").length) {
-              PLAYER.mediaType = "";
-              PLAYER.mediaId = "";
-              socket.emit("playerReady")
-          }
-          handleWindowResize()
+          
       })
   },
   createStyle: function(body) {
