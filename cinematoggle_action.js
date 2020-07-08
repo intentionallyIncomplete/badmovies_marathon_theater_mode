eddfc32c67c3c7bd43515d9d9209426b302d59b5
Ian <ibryan@uw.edu>
2020-07-07 22:17:51 -0700

    Changes

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,7 +1,13 @@
 <script>
 
-var cinematoggle_button = document.createElement('div');
-cinematoggle_button.HTMLElementObject.id = "cinematoggle";
+// var cinematoggle_button = document.createElement('div');
+// cinematoggle_button.HTMLElementObject.id = "cinematoggle";
+//
+// var main_section = document.getElementById("wrap").appendChild(cinematoggle_button);
 
-var main_section = document.getElementById("wrap").appendChild(cinematoggle_button);
+var cinematoggle_button = $("<div></div>")
+
+$("body").append(cinematoggle_button)
+
+cinematoggle_button.HTMLElementObject.id = "cinematoggle"
 </script>
