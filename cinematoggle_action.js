6b65f9ca87eb03973b80f595a244b45af299e4fd
Ian <ibryan@uw.edu>
2020-07-07 21:59:33 -0700

    Not working

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -3,4 +3,5 @@
 var cinematoggle_button = document.createElement('div');
 cinematoggle_button.HTMLElementObject.id = "cinematoggle";
 
+var main_section = document.getElementById("wrap").appendChild(cinematoggle_button);
 </script>
