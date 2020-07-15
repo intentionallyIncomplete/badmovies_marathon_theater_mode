2d2e4715df1b18d5fdc064c616308dec9d07eb2f
Ian <ibryan@uw.edu>
2020-07-15 02:11:07 -0700

    Just Messing with Scripts Now
    I'm going to bed.

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,21 +1,11 @@
 var cinemaToggleDiv = $("<div id='cinemaToggleDiv'><span class='glyphicon glyphicon-new-window'></span></div>");
 
-$("body").append(cinemaToggleDiv);
-
-createButtons: function() {
-            $('a[onclick*="removeVideo"]').parent().parent().append($("<li>").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", 'javascript:$("#cinematoggle").click()').text("Cinema Mode")));
-            $('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>').appendTo("body").click(function() {
-                if (!$("body").hasClass("cinemachat")) {
-                    if ($("#userlist").is(":visible")) {
-                        $("#userlisttoggle").click()
-                    }
-                }
-                $("body").toggleClass("cinemachat");
-                if ($("iframe[src*=livestream]").length) {
-                    PLAYER.mediaType = "";
-                    PLAYER.mediaId = "";
-                    socket.emit("playerReady")
-                }
-                handleWindowResize()
-            })
+$("body").append(cinemaToggleDiv).click(function() {
+    if (!$("body").hasClass("cinemachat")) {
+        if ($("#userlist").is(":visible")) {
+            $("#userlisttoggle").click()
         }
+    }
+    $("body").toggleClass("cinemachat");
+    handleWindowResize()
+});
