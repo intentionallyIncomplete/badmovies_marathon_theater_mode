b20094c1d1ed9bbd46931b6fb0d3da029748865d
Ian <ibryan@uw.edu>
2020-07-14 18:26:23 -0700

    Some things are working
    Got the CSS to render correctly using '!important! but im sure there is a better way to do that.

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,21 +1,3 @@
-<script>
+var cinemaToggleDiv = $("<div id='cinemaToggleDiv'><span class='glyphicon glyphicon-new-window'></span></div>");
 
-var cinematoggle_button = document.createElement('div');
-var icon_span = document.createElement('span');
-
-icon_span.className = "glyphicon glyphicon-new-window";
-cinematoggle_button.appendChild(icon_span);
-
-cinematoggle_button.HTMLElementObject.id = "cinematoggle";
-
-var body = document.getElementByTagName("body").appendChild(cinematoggle_button);
-
-$('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>')
-
-
-
-this.style = $("<style>").attr("type", "text/css").attr("id", "cinemaStyle").html(body).appendTo("head")
-
-
-
-</script>
+$("body").append(cinemaToggleDiv);
