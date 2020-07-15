799f03dc68ad81167e10c89c5b538d7ea824e391
Ian <ibryan@uw.edu>
2020-07-15 14:13:32 -0700

    Almost Working

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,11 +1,17 @@
-var cinemaToggleDiv = $("<div id='cinemaToggleDiv'><span class='glyphicon glyphicon-new-window'></span></div>");
-
-$("body").append(cinemaToggleDiv).click(function() {
-    if (!$("body").hasClass("cinemachat")) {
-        if ($("#userlist").is(":visible")) {
-            $("#userlisttoggle").click()
-        }
-    }
-    $("body").toggleClass("cinemachat");
-    handleWindowResize()
-});
+$('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>').appendTo("body").click(function() {
+                if (!$("body").hasClass("cinemachat")) {
+                    if ($("#userlist").is(":visible")) {
+                        $("#userlisttoggle").click()
+                    }
+                }
+                $("body").toggleClass("cinemachat");
+                if ($("iframe[src*=livestream]").length) {
+                    PLAYER.mediaType = "";
+                    PLAYER.mediaId = "";
+                    socket.emit("playerReady")
+                }
+                handleWindowResize()
+            })
+        },
+        createStyle: function(body) {
+            this.style = $("<style>").attr("type", "text/css").attr("id", "cinemaStyle").html(body).appendTo("head")
