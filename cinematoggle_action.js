dbbf5cb37c7782ff9763eb45b5714c37cb2e0fcb
Ian <ibryan@uw.edu>
2020-07-15 02:08:04 -0700

    Button Placed - Needs Function

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,3 +1,21 @@
 var cinemaToggleDiv = $("<div id='cinemaToggleDiv'><span class='glyphicon glyphicon-new-window'></span></div>");
 
 $("body").append(cinemaToggleDiv);
+
+createButtons: function() {
+            $('a[onclick*="removeVideo"]').parent().parent().append($("<li>").append($("<a>").attr("href", "javascript:void(0)").attr("onclick", 'javascript:$("#cinematoggle").click()').text("Cinema Mode")));
+            $('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>').appendTo("body").click(function() {
+                if (!$("body").hasClass("cinemachat")) {
+                    if ($("#userlist").is(":visible")) {
+                        $("#userlisttoggle").click()
+                    }
+                }
+                $("body").toggleClass("cinemachat");
+                if ($("iframe[src*=livestream]").length) {
+                    PLAYER.mediaType = "";
+                    PLAYER.mediaId = "";
+                    socket.emit("playerReady")
+                }
+                handleWindowResize()
+            })
+        }
