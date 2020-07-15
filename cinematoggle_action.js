764d73ab7811b7fdfa0273f4ce32139852a74dbf
Ian <ibryan@uw.edu>
2020-07-14 17:38:02 -0700

    Not Working Yet

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -1,26 +1,21 @@
 <script>
 
-// var cinematoggle_button = document.createElement('div');
-// var icon_span = document.createElement('span');
-//
-// icon_span.className = "glyphicon glyphicon-new-window";
-// cinematoggle_button.appendChild(icon_span);
-//
-// cinematoggle_button.HTMLElementObject.id = "cinematoggle";
-//
-// var body = document.getElementByTagName("body").appendChild(cinematoggle_button);
+var cinematoggle_button = document.createElement('div');
+var icon_span = document.createElement('span');
 
-function toggleCinema () {
-  createButtons: function() {
-      $('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>').appendTo("body").click(function() {
+icon_span.className = "glyphicon glyphicon-new-window";
+cinematoggle_button.appendChild(icon_span);
 
-          $("body").toggleClass("cinemachat");
-          
-      })
-  },
-  createStyle: function(body) {
-      this.style = $("<style>").attr("type", "text/css").attr("id", "cinemaStyle").html(body).appendTo("head")
-  }
-}
+cinematoggle_button.HTMLElementObject.id = "cinematoggle";
+
+var body = document.getElementByTagName("body").appendChild(cinematoggle_button);
+
+$('<div id="cinematoggle"><span class="glyphicon glyphicon-new-window "></span></div>')
+
+
+
+this.style = $("<style>").attr("type", "text/css").attr("id", "cinemaStyle").html(body).appendTo("head")
+
+
 
 </script>
